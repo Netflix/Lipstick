c99057b6049e296ed88a1824c66787c5e520167c
Mark Roddy <markroddy@gmail.com>
2013-08-12 18:11:43 -0400

    BasicP2LClient doesn't require a LipstickPigServer, user can pass in the PigContext object directly

--- lipstick-console/src/test/java/com/netflix/lipstick/pigtolipstick/BasicP2LClientTest.java
+++ lipstick-console/src/test/java/com/netflix/lipstick/pigtolipstick/BasicP2LClientTest.java
@@ -25,34 +25,67 @@ import org.apache.pig.newplan.Operator;
 
 public class BasicP2LClientTest {
 
-    @Test
-    public void testCreatePlanClientSaved() throws Exception {
-        PigStatusClient status_client = mock(PigStatusClient.class);        
-        BasicP2LClient client = new BasicP2LClient(status_client);
-        
-        LipstickPigServer server = mock(LipstickPigServer.class);
-        PigContext ctx = mock(PigContext.class);
+    /* Create a Mock PigContext object with methods stubbed out to
+       let the BasicP2LClient.createPlan() method complete */
+    public PigContext makeMockContext() {
         HExecutionEngine exec_engine = mock(HExecutionEngine.class);
         when(exec_engine.getLogToPhyMap()).thenReturn(new HashMap<Operator, PhysicalOperator>());
+
+        PigContext ctx = mock(PigContext.class);
         when(ctx.getExecutionEngine()).thenReturn(exec_engine);
         when(ctx.getProperties()).thenReturn(new Properties());
-        when(server.getPigContext()).thenReturn(ctx);
-        client.setPigServer(server);
+        return ctx;
+    }
 
+    /* Create Mock P2jPlanGenerator objects with methods stubbed out to
+       let the BasicP2LClient.createPlan() method complete, and
+       pass them to the client via the setter method. */
+    public void setMockPlanGenerators(BasicP2LClient client) {
         P2jPlan pln = mock(P2jPlan.class);
         P2jPlanGenerator unopt_plan = mock(P2jPlanGenerator.class);
         when(unopt_plan.getP2jPlan()).thenReturn(pln);
         P2jPlanGenerator opt_plan = mock(P2jPlanGenerator.class);
         when(opt_plan.getP2jPlan()).thenReturn(pln);
         client.setPlanGenerators(unopt_plan, opt_plan);
-
-        MROperPlan plan = mock(MROperPlan.class);
-        when(plan.iterator()).thenReturn(new ArrayList<MapReduceOper>().iterator());
-        client.createPlan(plan);
-
-        /* the client.savePlan() method should get called */
-        verify(status_client).savePlan(any(P2jPlanPackage.class));
-        
     }
 
+    public MROperPlan makeMockMROperPlan() {
+        MROperPlan plan = mock(MROperPlan.class);
+        when(plan.iterator()).thenReturn(new ArrayList<MapReduceOper>().iterator());
+        return plan;
+    }
+
+    @Test
+    public void testCreatePlanClientSaved() throws Exception {
+        PigStatusClient status_client = mock(PigStatusClient.class);        
+        BasicP2LClient client = new BasicP2LClient(status_client);
+        
+        LipstickPigServer server = mock(LipstickPigServer.class);
+        PigContext ctx = makeMockContext();
+        when(server.getPigContext()).thenReturn(ctx);
+        client.setPigServer(server);
+
+        setMockPlanGenerators(client);
+
+        client.createPlan(makeMockMROperPlan());
+
+        /* the client.savePlan() method should have been called */
+        verify(status_client).savePlan(any(P2jPlanPackage.class));        
+    }
+
+    @Test
+    public void testCreatePlanNoPigServerJustContext() throws Exception {
+        PigStatusClient status_client = mock(PigStatusClient.class);        
+        BasicP2LClient client = new BasicP2LClient(status_client);
+        
+        PigContext ctx = makeMockContext();
+        client.setPigContext(ctx);
+
+        setMockPlanGenerators(client);
+
+        client.createPlan(makeMockMROperPlan());
+
+        /* the client.savePlan() method should have been called */
+        verify(status_client).savePlan(any(P2jPlanPackage.class));        
+    }
 }
