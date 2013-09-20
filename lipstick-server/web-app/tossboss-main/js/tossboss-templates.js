d49ccdaa1cee55e0213db7f9cc1577f5fd8f04bc
Mark Roddy <markroddy@gmail.com>
2013-09-20 16:57:50 -0400

    make sure time parts formatted with two digits

--- lipstick-server/web-app/tossboss-main/js/tossboss-templates.js
+++ lipstick-server/web-app/tossboss-main/js/tossboss-templates.js
@@ -184,7 +184,7 @@ mrJobsTmpl: ' \
             <% } %> \
             <% if ((mrJobInfo.runData.startTime) && (mrJobInfo.runData.finishTime)) { %> \
               <% var duration = moment().startOf("day").add("ms", (mrJobInfo.runData.finishTime - mrJobInfo.runData.startTime)) %> \
-              <td><%= duration.format("H:m:s") %></td> \
+              <td><%= duration.format("HH:mm:ss") %></td> \
             <% } else { %> \
               <td>--</td> \
             <% } %> \
