7adc3edc99fa421a5e4079bc9863f43e2f27ca77
Jacob Perkins <jacob.a.perkins@gmail.com>
2014-06-04 09:16:11 -0700

    get schema for inserted foreach ops

--- lipstick-console/src/main/java/com/netflix/lipstick/adaptors/POJsonAdaptor.java
+++ lipstick-console/src/main/java/com/netflix/lipstick/adaptors/POJsonAdaptor.java
@@ -1,21 +1,24 @@
 package com.netflix.lipstick.adaptors;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.PhysicalOperator;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.expressionOperators.ConstantExpression;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.expressionOperators.POUserFunc;
 import org.apache.pig.backend.hadoop.executionengine.physicalLayer.plans.PhysicalPlan;
-import org.mortbay.log.Log;
+import org.apache.pig.backend.hadoop.executionengine.physicalLayer.relationalOperators.POForEach;
+import org.apache.pig.data.DataType;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.netflix.lipstick.model.operators.P2jLogicalRelationalOperator;
+import com.netflix.lipstick.model.operators.elements.SchemaElement;
 
 /**
  * Translates a PhysicalPlan to a map of p2jLogicalRelationalOperators.
- * The translation is a best guess and will drop operators if it doesn't
- * know what to do with them.
  * 
  * @author jacobp
  *
@@ -59,13 +62,61 @@ public class POJsonAdaptor {
         return map;
 	}
 	
+	protected String getTypeString(byte type) {
+	    String result = null;
+	    switch (type) {	        
+	    case DataType.TUPLE:
+	        result = "tuple()";
+	        break;
+	    case DataType.BAG:
+	        result = "bag{}";
+	        break;
+	    default:
+	        result = DataType.findTypeName(type);
+	        break;
+	    }
+	    return result;
+	}
+	
+	protected String translateInnerPlan(PhysicalPlan plan) {
+	    StringBuilder result = new StringBuilder();
+	    if (plan.getLeaves().size() > 0) {
+	        PhysicalOperator op = plan.getLeaves().get(0);
+	        String alias = null;
+	        if (op instanceof POUserFunc) {	            
+	            alias = ((POUserFunc)op).getFunc().getClass().getName();
+	        } else {
+	            alias = op.getClass().getSimpleName();
+	        }	        
+	        result.append(alias.replaceAll("\\.", "_"));
+	        result.append(":");
+	        result.append(getTypeString(op.getResultType()));
+	    }
+	    return result.toString();
+	}
+	
 	protected P2jLogicalRelationalOperator translateOperator(String id, PhysicalOperator op) {
 		P2jLogicalRelationalOperator p2jOp = new P2jLogicalRelationalOperator();
 		p2jOp.setUid(id);
-		p2jOp.setAlias(op.getAlias() != null ? op.getAlias() : "{tez-op}");		
+		p2jOp.setAlias(op.getAlias() != null ? op.getAlias() : "{tez-inserted-op}");		
 		p2jOp.setOperator(op.getClass().getSimpleName());
-		p2jOp.setMapReduce(jobId, "TezVertex");
+		p2jOp.setMapReduce(jobId, "TezVertexInserted");
 		p2jOp.setLocation(new P2jLogicalRelationalOperator.Location(0, "", new ArrayList<String>()));
+		if (op instanceof POForEach) {
+		    List<PhysicalPlan> innerPlans = ((POForEach)op).getInputPlans();
+		    StringBuilder sb = new StringBuilder();
+		    sb.append("(");
+		    Iterator<PhysicalPlan> it = innerPlans.iterator();
+		    while (it.hasNext()) {
+		        PhysicalPlan innerPlan = it.next();
+		        sb.append(translateInnerPlan(innerPlan));
+		        if (it.hasNext()) {
+		            sb.append(",");
+		        }
+		    }
+		    sb.append(")");
+		    p2jOp.setSchemaString(sb.toString());
+		}
 		
 		List<String> successors = Lists.newArrayList();
 		List<String> predecessors = Lists.newArrayList();
@@ -82,6 +133,7 @@ public class POJsonAdaptor {
 		
 		p2jOp.setSuccessors(successors);
 		p2jOp.setPredecessors(predecessors);
+		
 		return p2jOp;
 	}
 	
